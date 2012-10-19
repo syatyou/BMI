f6e1a446ceba66eb6c9923cb209e9c129ee236cb
itacademy <itacademy@192.168.1.11>
2012-10-19 21:27:41 +0900

    tukareta

---
+++ src/bmi/bmi.java
@@ -0,0 +1,37 @@
+package bmi;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class bmi
+ */
+@WebServlet("/bmi")
+public class bmi extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException {
+		
+		double weight;
+		double height;
+		try {
+			weight = Double.parseDouble(req.getParameter("weight"));
+			height = Double.parseDouble(req.getParameter("height"));
+		} catch (Exception e) {
+			req.setAttribute("error", "ÉpÉâÉÅÅ[É^Ç™ê≥ÇµÇ≠Ç†ÇËÇ‹ÇπÇÒ");
+			req.getRequestDispatcher("/WEB-INF/mvc/bmi/error.jsp")
+			    .forward(req, res);
+			return;
+		}
+		
+	    Person person = new Person(height, weight);
+	    req.setAttribute("person", person);
+
+	    req.getRequestDispatcher("/WEB-INF/mvc/bmi/result.jsp")
+	        .forward(req, res);          
+	}
+}
